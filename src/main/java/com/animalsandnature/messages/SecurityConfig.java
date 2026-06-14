987fab8a80982183575dab5d2864f92dff75f1e3
Valencia <starrv2011@gmail.com>
2026-06-14 08:09:12 -0400

    updated security config and controller

--- src/main/java/com/animalsandnature/messages/SecurityConfig.java
+++ src/main/java/com/animalsandnature/messages/SecurityConfig.java
@@ -2,14 +2,14 @@ package com.animalsandnature.messages;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
