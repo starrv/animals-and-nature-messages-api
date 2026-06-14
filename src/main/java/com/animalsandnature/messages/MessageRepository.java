1cce21003258b06f2b84a62f895181cf79352075
Valencia <starrv2011@gmail.com>
2026-06-14 14:56:18 -0400

    updates

--- src/main/java/com/animalsandnature/messages/MessageRepository.java
+++ src/main/java/com/animalsandnature/messages/MessageRepository.java
@@ -1,19 +1,16 @@
 package com.animalsandnature.messages;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public interface MessageRepository extends MongoRepository<Message,String> {
+public interface MessageRepository extends MongoRepository<Message, String> {
 
     Message findMessageByIdAndNotificationType(String id,String notificationType);
 
     Message save(Message msg);
 
-    @Query(value="{notificationType: '?0'}")
     List<Message> findMessagesByNotificationType(String notificationType);
 
-    @Override
     long count();
 }
