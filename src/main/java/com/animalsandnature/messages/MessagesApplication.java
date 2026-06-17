5bb38b65ddfbe3869a403034b9de591b4c6b5ff9
Valencia <starrv2011@gmail.com>
2026-06-16 21:07:14 -0400

    minor changes and deleted unnecessary packages

--- src/main/java/com/animalsandnature/messages/MessagesApplication.java
+++ src/main/java/com/animalsandnature/messages/MessagesApplication.java
@@ -1,12 +1,9 @@
 package com.animalsandnature.messages;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class MessagesApplication{
 
 	public static void main(String[] args) {
